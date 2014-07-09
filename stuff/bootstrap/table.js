06c9897fdbb0a1756a8de9a75cec6dbabccd26f0
Daniel Baumann <mail@daniel-baumann.ch>
2014-07-09 08:37:32 +0200

    Updating to bootstrap version 3.2.0.

--- stuff/bootstrap/table.js
+++ stuff/bootstrap/table.js
@@ -1,3 +1,3 @@
 $(function() {
-$("table").addClass("table table-bordered table-hover table-responsive table-striped");
+  $("table").addClass("table table-striped table-bordered table-hover table-condensed table-responsive");
 });
